1202b044549762c8a0a97b97aef1e52189ac10bf
chenjie <deepstarry@vip.sina.com>
2020-06-13 15:55:36 +0800

    Complete example if WTL.

---
+++ C5_FileUIOperationAndEvent/WTL/COpenCVTestView.cpp
@@ -0,0 +1,9 @@
+//
+//  COpenCVTestView.cpp
+//  WTL
+//
+//  Created by chenjie on 2020/6/13.
+//  Copyright © 2020 chenjie. All rights reserved.
+//
+
+#include "COpenCVTestView.hpp"
---
+++ C5_FileUIOperationAndEvent/WTL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  WTL
+//
+//  Created by chenjie on 2020/6/13.
+//  Copyright © 2020 chenjie. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
